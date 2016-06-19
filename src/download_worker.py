1abcc51f8e99801879c30d102eac11999bbd24a8
Real Limoges <b.real.limoges@gmail.com>
2016-06-18 21:09:40 -0700

    refactor download_worker.py

--- src/download_worker.py
+++ src/download_worker.py
@@ -1,8 +1,10 @@
 import boto3
-import multiprocessing
+from multiprocessing import Pool, freeze_support
 import pymongo
 import os
 import urllib2
+import sys
+import itertools
 
 client = pymongo.MongoClient()
 db = client['flickr_links']
@@ -13,17 +15,40 @@ s3 = boto3.resource('s3')
 s3.create_bucket(Bucket=bucket)
 
 
-def download_image(link):
+def unpack_func(a_b):
+    '''
+    INPUT: Tuple of arguments to pass to function
+    OUTPUT : Returns Download Image(Link, Bucket)
+    '''
+    return download_image(*a_b)
+
+def download_image(link, bucket):
+    """
+    INPUT: Link (string), Bucket (string)
+    OUTPUT: None
+
+    Opens the link and downloads the file to disk. Uploads to s3
+    """
+    return link
+
     response = urllib2.urlopen(link)
     with open(link[36:], 'wb') as outfile:
         outfile.write(response.read())
     s3.Object(bucket, link[36:]).put(Body=open(link[36:]))
     os.remove(link[36:])
-    print link
 
 if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        coll_name = sys.argv[1]
+        bucket_name = sys.argv[2]
+    else:
+        print """Please provide a string for the MongoDB collection and a string for the s3 bucket"""
+        sys.exit(1)
+    
+    s3.create_bucket(Bucket=bucket_name)
+    collection = db[coll_name]
 
     url_list = [link['_id'] for link in collection.find()]
-    pool = multiprocessing.Pool()
+    pool = Pool()
 
-    results = pool.map(download_image, url_list)
+    results = pool.map(unpack_fun, itertools.izip(url_list, itertools.repeat(bucket_name)
