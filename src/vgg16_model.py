f4371ae1a55b1afe60ca91ee09e4aa9dfd6febe2
Real Limoges <b.real.limoges@gmail.com>
2016-06-21 12:44:35 -0700

    refactor vgg16_model.py

--- src/vgg16_model.py
+++ src/vgg16_model.py
@@ -6,7 +6,30 @@ from keras.optimizers import SGD
 import numpy as np
 from skimage import io
 import cv2
+import os
+import pandas as pd
 
+def pop_layer(model):
+    '''
+    INPUTS: Keras Sequential Model Object
+    OUTPUTS: None (Side Effect of Removing a Keras Layer)
+
+    Takes a model as an input. If model doesn't have enough layers,
+    raises exception. Clears layers after popping layer off.
+    '''
+    
+    if not model.outputs:
+        raise Exception("Model does not have enough layers to pop")
+
+    model.layers.pop()
+    if not model.layers:
+        model.outputs = []
+        model.inbound_nodes=[]
+        model.outbound_nodes=[]
+    else:
+        model.layers[-1].outbound_nodes = []
+        model.outputs = [model.layers[-1].output]
+    model.built = False
 
 def VGG_16(weights):
     '''
@@ -69,29 +92,49 @@ def VGG_16(weights):
     model.add(Dense(1000, activation='softmax'))
 
     # Load Weights and Drop Classification Layer
-    model.load_weights(weights_path)
-    model.layers.pop()
-
+    model.load_weights(weights)
+   
+    #Pop off Classification Layer and Final Dropout
+    pop_layer(model) 
+    pop_layer(model)
+    
     return model
 
 def transform_image(path):
-    im = cv2.resize(cv2.(imread(path), (224, 224)).astype(np.float32)
+    '''
+    INPUTS: Path to Image (String)
+    OUTPUTS: 4D Numpy Tensor
 
-    # Mean pixel values from original training set
-    im[:,:,0] -= 103.939
-    im[:,:,1] -= 115.779
-    im[:,:,2] -= 123.68
-
+    Opens file with cv2, transposes to RGB, adds dimension and returns
+    '''
+    im = cv2.imread(path)
     im = im.transpose((2,1,0))
     return np.expand_dims(im, axis=0)
 
+def create_weights(model):
+    ''' 
+    INPUTS: Model (Compiled Keras NN Function)
+    OUTPUTS: Side Effects (Writes Pandas Dataframe to disk)
+
+    Takes a compiled Keras NN function and feeds each image in
+    given directory to the neural network. Output from each
+    image is a (1,4096) numpy array, each of which is the weight
+    on a neuron on the final connected layer. Writes these to file.
+    '''
+    data = pd.DataFrame()
+    for x, image in enumerate(os.listdir('images/')):
+        print "processing image {}".format(x)
+        if image.endswith('.jpg'):
+            img_arr = transform_image(os.getcwd() + '/images/' + image)
+            output = model.predict(img_arr)[0]
+            output.shape = (1,4096)
+            data = pd.concat([data, pd.DataFrame(output)], axis=0)
+
+    return data
 if __name__ == "__main__":
 
-    model = VGG_16('../data/vgg16_weights.h5')
+    model = VGG_16('../transfer_learning/data/vgg16_weights.h5')
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy')
-
-    im = transform_image('../test_image.jpg')
-    out = model.predict(im)
-    print out
-    
+    data = create_weights(model)
+    data.to_csv("cluster.csv", index=False)
